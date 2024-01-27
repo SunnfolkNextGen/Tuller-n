89ffe6fb1309a4df745a5575db7c138e15a56b89
AslakDB <Askelakken@gmail.com>
2024-01-27 01:53:53 +0100

    Added playermovement, PlayerInputActions and playerLook, We now have a base player character

---
+++ Assets/Scripts/InputActionsController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class InputActionsController : MonoBehaviour
+{
+    //private InputSystem _inputSystem;
+    private CharacterController _characterController;
+    private PlayerInput _playerInput;
+    public Vector2 MoveVector { get; private set; }
+    public Vector2 LookVector { get; private set; }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _playerInput = new PlayerInput();
+    }
+
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MoveVector = _playerInput.Input.Movement.ReadValue<Vector2>();
+        LookVector = _playerInput.Input.Look.ReadValue<Vector2>();
+    }
+}
