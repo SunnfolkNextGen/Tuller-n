b09642b6580a689d6ef1fbab001b7823d6b044c7
Haakon 1 <114143581+N4kras@users.noreply.github.com>
2024-01-28 14:51:37 +0100

    FINAL PUSH

--- Assets/PersonalAngel.cs
+++ Assets/PersonalAngel.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PersonalAngel : MonoBehaviour
 {
 
+    [SerializeField] private Transform corner1, corner2;
+
     private GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,11 @@ public class PersonalAngel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 8,
-            player.transform.position.z);
+
+        float newX = Mathf.Clamp(player.transform.position.x, corner2.position.x, corner1.position.x);
+        
+        float newZ = Mathf.Clamp(player.transform.position.z, corner1.position.z, corner2.position.z);
+        
+        transform.position = new Vector3(newX, player.transform.position.y + 8,newZ);
     }
 }
